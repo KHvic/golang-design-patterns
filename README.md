540f73c15611a27a1dd03bf66776f0a2d4d0e582
Ka Hian <notesalt@hotmail.com>
2019-12-18 22:48:30 +0800

    singleton add desc

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # golang-design-patterns
+
+## Creational
+Singleton
+
+## Structural
+Structural
+
+## Behavioral
+
+## Reference
+- https://java-design-patterns.com/
+- https://github.com/iluwatar/java-design-patterns
--- Singleton/main.go
+++ Singleton/main.go
@@ -3,8 +3,9 @@ package singleton
 /*
 	Type: Creational
 	Purpose: Ensure a class only has one instance, and provide a global point of access to it
-	Additional: Global variables are bad because it can be affected by code from anywhere
-				Make testing harder
+	Additional: - Global variables are bad because it can be affected by code from anywhere
+				- Global shared instance which prevents an object and resources used by this object from being deallocated
+				- Creates tightly coupled code. The clients of the Singleton become difficult to test
 */
 
 import "sync"
@@ -12,12 +13,12 @@ import "sync"
 var (
 	once sync.Once
 
-	instance []int
+	singleton []int
 )
 
 func Get() []int {
 	once.Do(func() {
-		instance = make([]int, 0, 0)
+		singleton = make([]int, 0, 0)
 	})
-	return instance
+	return singleton
 }
